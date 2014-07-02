b3088348ea71a1b7d02b320ce242c0817c1d2865
oguzhan.cansever <oguzhan.cansever@meinestadt.de>
2014-07-02 23:40:42 +0200

    politify

---
+++ config.rb
@@ -0,0 +1,13 @@
+http_path = "/assets/"
+css_dir = "/assets/css"
+sass_dir = "/assets/sass"
+images_dir = "/assets/img" # Sprite src images
+generated_images_dir = "/assets/img" # Generated sprites source
+
+relative_assets = true
+line_comments = false
+color_output = false
+preferred_syntax = :scss
+output_style = :compressed # :expanded or :nested or :compact or :compressed
+
+
