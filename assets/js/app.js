bae93a8a4e1925a9a3e0419a3aaaf9e109b70a6e
oguzhan.cansever <oguzhan.cansever@meinestadt.de>
2014-07-05 00:41:47 +0200

    ..

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,12 @@
 
 $(document).ready(function() {
   $('.facebook-link').magnificPopup({type:'image'});
+
+  $('.contact').on('click', function() {
+    var $target = $('html,body'); 
+    $target.animate({scrollTop: $target.height()}, 1000);
+  });
+
+  $('.hyper').hyphenate('de');
+
 });
